3fdf18b3f400d8c2bc0b5683473c01415ac6a0ef
Julián Rincón Morales <julianrinconm@gmail.com>
2020-11-02 19:59:21 +0100

    Añadido directorio img a webpack para la copia Añadido boton con el dado Aparece el dado

--- src/js/main.js
+++ src/js/main.js
@@ -32,12 +32,41 @@ function compruebaMsg(msg){
             //Lo añado al cuerpo
             $("body").append(nuevoDiv);
         }
-
         // Pongo el texto pedido
         $("#nuevoDiv").html("A luchar héroe :" + $("#nombre").val());
         $("#jugar").prop('disabled',false);
+        // Cambio boton enviar por boton tirar dado
+        cambiaDado();
     } else {
         // Solo hay 2 opciones, pero por si acaso
         alert("Opción no contemplada");
     }
+}
+// Funcion que cambia el boton enviar por un boton "Dado"
+function cambiaDado(){
+    //Quito el listener
+    $("#enviar").off("click");
+    // Le cambio el id para que sea acorde con la accion
+    $("#enviar").attr("id","tirarDado");
+    // Le cambio el texto
+    //$("#tirarDado").attr("value","Tirar dado");
+    //Quito el texto, no me gusta
+    $("#tirarDado").attr("value","");
+    // Le pongo imagen 
+    $("#tirarDado").css("background-image","url(Alea_1.png)");
+    // No quiero multiplcarla
+    $("#tirarDado").css("background-repeat","no-repeat");
+    // Quiero que cubra el boton
+    $("#tirarDado").css("background-size","cover");
+    // Hago el botón de un tamaño razonable
+    $("#tirarDado").css("height","8rem");
+    $("#tirarDado").css("width","8rem");
+    // Le quito el borde y el color al boton
+    $("#tirarDado").css("border","none");
+    $("#tirarDado").css("background-color","none");
+    // Le pongo un listener nuevo
+    $("#tirarDado").click(tiraDado);
+}
+function tiraDado(){
+    alert("He tirado dado");
 }
